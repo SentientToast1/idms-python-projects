36fd3927fb4b76000b3e7c4d8ed962f9446e0fe6
SentientToast1 <dawonder113@gmail.com>
2025-06-18 01:12:04 +0530

    updated readme and added json related programs

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # idms-python-projects
 projects for 3 week internship at IDMS
 
-# program 1
+# program 1 "web-scraping.py"
 Extracts the top 5 news and their headlines and stores them in a txt file
 
-# program 2
+# program 2 "sheet-automation.py"
 A simple Excel-based intern manager that lets you add, search, modify, and delete intern records via terminal.
 
-# program 3
+# program 3 "csv-sales-report.py"
 reads a sales csv file, removes blank or incomplete entries and outputs a report of relevant information
+
+# program 4 "weather-json.py"
+gets weather data in JSON form, goes through a list of cities and outputs relevant data to a .csv
---
+++ weather-json.py
@@ -0,0 +1,44 @@
+import requests, csv
+
+
+APIKEY = "4becc19e59e7ea573149a58953bfaa4e"
+OUTPUTCSV = "temperature.csv"
+
+def main():
+    if APIKEY != "":
+        cities = ["London","Austin","Paris","Jakarta"]
+        location = []
+        weatherDescript = []
+        feelsLikeTemp = []
+        huimidity = []
+        ptr = 0
+        for x in cities:
+            #gets latitude and longitude
+            geoCodeUrl = f'http://api.openweathermap.org/geo/1.0/direct?q={x}&appid={APIKEY}'
+            response = requests.get(geoCodeUrl)
+            geocodeData = response.json()
+            location.append((geocodeData[0]["lat"],geocodeData[0]["lon"]))
+            #gets the weather
+            getWeatherUrl = f'https://api.openweathermap.org/data/2.5/weather?lat={location[ptr][0]}&lon={location[ptr][1]}&appid={APIKEY}&units=metric'
+            response = requests.get(getWeatherUrl)
+            weatherData = response.json()
+            weatherDescript.append(weatherData["weather"][0]["description"])
+            feelsLikeTemp.append(weatherData["main"]["feels_like"])
+            huimidity.append(weatherData["main"]["humidity"])
+            ptr += 1
+
+        finalRows = [["city","Feels Like","Humidity"]]
+
+        for x in range(len(cities)):
+            finalRows.append([cities[x],str(feelsLikeTemp[x])+"C",str(huimidity[x])+"%"])
+
+
+        with open(OUTPUTCSV,'w',newline="") as file:
+            writer = csv.writer(file)
+            writer.writerows(finalRows)
+
+        print("done!")
+    else:
+        print("API key not entered")
+
+main()

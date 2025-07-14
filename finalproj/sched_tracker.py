5ae9b31788daaac1313ee65c69a75b8881336477
SentientToast1 <dawonder113@gmail.com>
2025-07-14 23:21:30 +0530

    schedule program completed

--- finalproj/sched_tracker.py
+++ finalproj/sched_tracker.py
@@ -1,5 +1,4 @@
-from datetime import datetime
-import csv
+from datetime import datetime,date
 import argparse
 import json
 import os
@@ -13,6 +12,7 @@ parser.add_argument("-name",type=str,help="name of the task")
 parser.add_argument("-list", action="store_true",help="list the tasks")
 parser.add_argument("-stoptask",action="store_true",help="stop the ongoing task")
 parser.add_argument("-ct",action='store_true',help="clears all tasks")
+parser.add_argument("-rep",action='store_true',help="generate report")
 args = parser.parse_args()
 currentTime  = datetime.now()
 
@@ -64,7 +64,27 @@ def listTasks():
         print(f'{data[i]["date"].ljust(12)}\t{data[i]["data"]["name"].ljust(20)}\t{data[i]["data"]["start"]}\t\t\t{data[i]["data"]["end"]}')
 
 def makeReport():
-    pass
+    data = []
+    with open("data/tasklist.json", 'r') as f:
+        data = json.load(f)
+    entries = len(data)
+    rows = [["Name","Duration"]]
+    for i in range(entries):
+        startVar = data[i]["data"]["start"]
+        endVar = data[i]["data"]["end"]
+        st = datetime.combine(date.today(),datetime.strptime(startVar,"%H:%M").time())
+        et = datetime.combine(date.today(),datetime.strptime(endVar,"%H:%M").time())
+        stts = st.timestamp()
+        etts = et.timestamp()
+        duration = int(etts-stts)
+        hrs = duration // 3600
+        mnt = (duration % 3600) // 60
+        scn = duration % 60
+        rows.append([data[i]["data"]["name"],f'{hrs}h {mnt}m {scn}s'])
+    for i in range(entries+1):
+        for j in range(2):
+            print(rows[i][j], end = ' ')
+        print("")
 
 def clearFile():
     with open("data/tasklist.json",'w') as f:
@@ -82,9 +102,10 @@ def main():
         listTasks()
     elif args.ct:
         clearFile()
+    elif args.rep:
+        makeReport()
     else:
         print("no arguments provided!")
-
 
 
 main()
